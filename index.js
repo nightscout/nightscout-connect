f604549b5d7083009bd020f6bae2292099277ed3
Ben West <bewest@gmail.com>
2023-04-07 09:38:12 -0700

    minimal, no-op Nightscout plugin
    Provide core framework to deliver features without modifying
    additional code in core Nightscout. This is a "hello world"
    implementation that is exposed to Nightscout internals without
    actually recording anything.

--- index.js
+++ index.js
@@ -1,24 +1,89 @@
 
+const { createMachine, Machine, actions, interpret, spawn  } = require('xstate');
 
 /*
 * 
 * https://github.com/nightscout/cgm-remote-monitor/blob/master/lib/server/bootevent.js
 *
   // ctx.nightscoutConnect = require('nightscout-connect')(env, ctx);
-  // ctx.nightscoutConnect.run( ).then(next).catch(next);
 */
+var axios = require('axios');
+var builder = require('./lib/builder');
+var sources = require('./lib/sources');
+var outputs = require('./lib/outputs');
 
+
+function internalLoop (input, output) {
+}
 
 function manage (env, ctx) {
 
   // source
   // output
   // env.extendedSettings.connect.source
+  var spec = { kind: 'disabled' };
+  if (!env.extendedSettings.connect) {
+    console.log("Skipping disabled nightscout-connect");
+    return;
+  }
+  if (!env.extendedSettings.connect.source) {
+    console.log("Skipping disabled nightscout-connect, no source driver spec");
+    return;
+  }
+
+  spec.kind = env.extendedSettings.connect.source;
+
+  var internal = { name: 'internal' };
+  var output = outputs(internal)(internal, ctx);
+  console.log("CONFIGURED OUTPUT", output);
+
+  // var things = internalLoop(input, output);
+  // everything known for output
+  // output must be passed into builder, before generate_driver is
+  // called.
+  var make = builder({ output });
+
+  // select an available input source implementation based on env
+  // variables/config
+  var driver = sources(spec);
+  var validated = driver.validate(env.extendedSettings.connect);
+  if (validated.errors) {
+      ctx.bootErrors.push(...validated.errors);
+  }
+
+  console.log("INPUT PARAMS", spec, validated.config);
+
+  if (!validated.ok) {
+    console.log("Invalid, disabling nightscout-connect", validated);
+    return;
+  }
+  var impl = driver(validated.config, axios);
+  impl.generate_driver(make);
+  var built = make( );
+  var things = built;
+  // console.log("BUILDER OUTPUT", JSON.stringify(built, null, 2));
+  // return built;
 
   // TODO: consider subscribing to ctx.bus for start/stop.
-  function handle ( ) { };
-  handle.run = () => Promise.resolve(handle);
-  handle.stop = () => Promise.resolve(handle);
+  function handle ( ) { return actor; };
+  handle.run = () => {
+    actor.send({type: 'START'});
+    return Promise.resolve(handle);
+  }
+  handle.stop = () => {
+    actor.stop( );
+    return Promise.resolve(handle);
+  }
+
+
+  // ctx.bus.on('finishBoot', console.log.bind(console, 'DEBUG nightscout-connect'));
+  ctx.bus.once('finishBoot', handle.run);
+  ctx.bus.once('tearDown', handle.stop);
+  // console.log(things);
+  var actor = interpret(things);
+  actor.start( );
+  // actor.send({type: 'START'});
+
   return handle;
 }
 
