218cdd777c2594a7844ef78463a77bd61ab06674
Ben West <bewest@gmail.com>
2023-04-10 08:39:25 -0700

    experimental capture mode
    Have the cycle machine set up a tracker for each frame to capture http
    interactions.

---
+++ commands/capture.js
@@ -0,0 +1,63 @@
+
+const { createMachine, Machine, actions, interpret, spawn  } = require('xstate');
+var testImpl = require('../testable_driver');
+var axios = require('axios');
+var builder = require('../lib/builder');
+var sources = require('../lib/sources');
+var outputs = require('../lib/outputs');
+
+function sidecarLoop (input, output, capture) {
+  
+  // everything known for output
+  // output must be passed into builder, before generate_driver is
+  // called.
+  var endpoint = outputs(output)(output, axios);
+  var make = builder({ output: endpoint, capture });
+  // var make = builder({ output });
+
+  // select an available input source implementation based on env
+  // variables/config
+  var driver = sources(input);
+  console.log("INPUT PARAMS", input);
+  var impl = driver(input, axios);
+  // var impl = testImpl.fakeFrame({ }, axios);
+
+  impl.generate_driver(make);
+
+  var built = make( );
+  // console.log("BUILDER OUTPUT", built);
+  console.log("BUILDER OUTPUT", JSON.stringify(built, null, 2));
+  return built;
+
+}
+
+function main (argv) {
+  console.log("STARTING", argv);
+  // selected output
+  // argv.nightscoutEndpoint;
+  // argv.apiSecret;
+  // 
+
+  var output = { name: 'nightscout', url: argv.nightscoutEndpoint, apiSecret: argv.apiSecret };
+  console.log("CONFIGURED OUTPUT", output);
+  var input = { kind: argv.source, url: argv.sourceEndpoint, apiSecret: argv.sourceApiSecret || '' };
+  console.log("CONFIGURED INPUT", input);
+
+  var things = sidecarLoop(input, output, { dir: argv.dir });
+  console.log(things);
+  var actor = interpret(things);
+  actor.start( );
+  actor.send({type: 'START'});
+  setTimeout(( ) => {
+  actor.send({type: 'STOP'});
+  }, 60000 * 1);
+
+}
+
+
+module.exports.command = 'capture <dir> [hint]';
+module.exports.describe = 'Runs as a background server forever.'
+module.exports.builder = (yargs) => yargs
+  .option('source', { alias: 'hint', describe: 'source input', default: 'default', choices: ['dexcomshare', 'default', 'nightscout' ]})
+  .option('dir', { describe: 'output directory', default: './har' })
+module.exports.handler = main;
